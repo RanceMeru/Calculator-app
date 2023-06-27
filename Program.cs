2d0775e518e917f331e607adf64147ab666387f1
Johnlouie Gutierrez <johgu.829@gmail.com>
2023-06-27 12:43:59 -0700

    new

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hi, welcome to my Calculator Console app ");
+        Console.WriteLine("Please enter any number or decimal number");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        double result = 0;
+
+        while (true)
+        {
+            Console.WriteLine("Would you like to add, subtract, multiply or divide?");
+
+            switch (Console.ReadLine())
+            {
+                case "add":
+                    result = num1 + num2;
+                    Console.WriteLine("{0}+{1}=" + "{2}", num1, num2, result);
+                    break;
+
+                case "subtract":
+                    result = num1 - num2;
+                    Console.WriteLine("{0}-{1}=" + "{2}", num1, num2, result);
+                    break;
+
+                case "multiply":
+                    result = num1 * num2;
+                    Console.WriteLine("{0}*{1}=" + "{2}", num1, num2, result);
+                    break;
+
+                case "divide":
+                    result = num1 / num2;
+                    Console.WriteLine("{0}/{1}=" + "{2}", num1, num2, result);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+
+            Console.WriteLine("Would you like to continue or exit? (Type 'continue' or 'exit')");
+            string choice = Console.ReadLine();
+
+            if (choice == "exit")
+            {
+                break;
+            }
+            else if (choice == "continue")
+            {
+                Console.WriteLine("Please enter any number or decimal number");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter the second number: ");
+                num2 = Convert.ToDouble(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+                break;
+            }
+        }
+    }
+}
